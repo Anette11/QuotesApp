fc72241d63b81b76f1d36257d37b30d30e0b5651
Anette11 <krivaltseva@gmail.com>
2022-11-23 15:07:14 +0300

    Move icon in HomeTopAppBar.kt from title{} to actions{}

--- app/src/main/java/com/example/quotesapp/ui/components/home/HomeTopAppBar.kt
+++ app/src/main/java/com/example/quotesapp/ui/components/home/HomeTopAppBar.kt
@@ -1,8 +1,5 @@
 package com.example.quotesapp.ui.components.home
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -12,9 +9,6 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -27,22 +21,10 @@ fun HomeTopAppBar(
     isMenuOpened: Boolean
 ) = TopAppBar(
     title = {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                modifier = Modifier.weight(1f),
-                text = stringResource(id = R.string.top_app_bar_quotes_name),
-                fontFamily = FontFamily(Font(R.font.rubik_regular))
-            )
-            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen._8dp)))
-            IconButton(
-                onClick = onClearCacheClick
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = stringResource(id = R.string.icon_delete_description)
-                )
-            }
-        }
+        Text(
+            text = stringResource(id = R.string.top_app_bar_quotes_name),
+            fontFamily = FontFamily(Font(R.font.rubik_regular))
+        )
     },
     navigationIcon = {
         IconButton(
@@ -59,6 +41,16 @@ fun HomeTopAppBar(
                     contentDescription = stringResource(id = R.string.icon_menu_description)
                 )
             }
+        }
+    },
+    actions = {
+        IconButton(
+            onClick = onClearCacheClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = stringResource(id = R.string.icon_delete_description)
+            )
         }
     }
 )
